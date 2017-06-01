4735dbd10aa0263c2b8d0521661c408a9cdc47c0
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-06-01 11:13:49 -0400

    final

--- kassakaappi/kassakaappi/kassakaappi.ino
+++ kassakaappi/kassakaappi/kassakaappi.ino
@@ -8,198 +8,198 @@ enum  direction { FORWARD, REVERSE };
 // NeoPattern Class - derived from the Adafruit_NeoPixel class
 class NeoPatterns : public Adafruit_NeoPixel
 {
-    public:
+  public:
 
-    // Member Variables:  
+    // Member Variables:
     pattern  ActivePattern;  // which pattern is running
     direction Direction;     // direction to run the pattern
-    
+
     unsigned long Interval;   // milliseconds between updates
     unsigned long lastUpdate; // last update of position
-    
+
     uint32_t Color1, Color2;  // What colors are in use
     uint16_t TotalSteps;  // total number of steps in the pattern
     uint16_t Index;  // current step within the pattern
-    
+
     void (*OnComplete)();  // Callback on completion of pattern
-    
+
     // Constructor - calls base-class constructor to initialize strip
     NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type, void (*callback)())
-    :Adafruit_NeoPixel(pixels, pin, type)
+      : Adafruit_NeoPixel(pixels, pin, type)
     {
-        OnComplete = callback;
+      OnComplete = callback;
     }
-    
+
     // Update the pattern
     void Update()
     {
-        if((millis() - lastUpdate) > Interval) // time to update
+      if ((millis() - lastUpdate) > Interval) // time to update
+      {
+        lastUpdate = millis();
+        switch (ActivePattern)
         {
-            lastUpdate = millis();
-            switch(ActivePattern)
-            {
-                case COLOR_WIPE:
-                    ColorWipeUpdate();
-                    break;
-                case SCANNER:
-                    ScannerUpdate();
-                    break;
-                default:
-                    break;
-            }
+          case COLOR_WIPE:
+            ColorWipeUpdate();
+            break;
+          case SCANNER:
+            ScannerUpdate();
+            break;
+          default:
+            break;
         }
+      }
     }
-  
+
     // Increment the Index and reset at the end
     void Increment()
     {
-        if (Direction == FORWARD)
+      if (Direction == FORWARD)
+      {
+        Index++;
+        if (Index >= TotalSteps)
         {
-           Index++;
-           if (Index >= TotalSteps)
-            {
-                Index = 0;
-                if (OnComplete != NULL)
-                {
-                    OnComplete(); // call the comlpetion callback
-                }
-            }
+          Index = 0;
+          if (OnComplete != NULL)
+          {
+            OnComplete(); // call the comlpetion callback
+          }
         }
-        else // Direction == REVERSE
+      }
+      else // Direction == REVERSE
+      {
+        --Index;
+        if (Index <= 0)
         {
-            --Index;
-            if (Index <= 0)
-            {
-                Index = TotalSteps-1;
-                if (OnComplete != NULL)
-                {
-                    OnComplete(); // call the comlpetion callback
-                }
-            }
+          Index = TotalSteps - 1;
+          if (OnComplete != NULL)
+          {
+            OnComplete(); // call the comlpetion callback
+          }
         }
+      }
     }
-    
+
     // Reverse pattern direction
     void Reverse()
     {
-        if (Direction == FORWARD)
-        {
-            Direction = REVERSE;
-            Index = TotalSteps-1;
-        }
-        else
-        {
-            Direction = FORWARD;
-            Index = 0;
-        }
+      if (Direction == FORWARD)
+      {
+        Direction = REVERSE;
+        Index = TotalSteps - 1;
+      }
+      else
+      {
+        Direction = FORWARD;
+        Index = 0;
+      }
     }
 
     // Initialize for a ColorWipe
     void ColorWipe(uint32_t color, uint8_t interval, direction dir = FORWARD)
     {
-        ActivePattern = COLOR_WIPE;
-        Interval = interval;
-        TotalSteps = numPixels();
-        Color1 = color;
-        Index = 0;
-        Direction = dir;
+      ActivePattern = COLOR_WIPE;
+      Interval = interval;
+      TotalSteps = numPixels();
+      Color1 = color;
+      Index = 0;
+      Direction = dir;
     }
-    
+
     // Update the Color Wipe Pattern
     void ColorWipeUpdate()
     {
-        setPixelColor(Index, Color1);
-        show();
-        Increment();
+      setPixelColor(Index, Color1);
+      show();
+      Increment();
     }
-    
+
     // Initialize for a SCANNNER
     void Scanner(uint32_t color1, uint8_t interval)
     {
-        ActivePattern = SCANNER;
-        Interval = interval;
-        TotalSteps = (numPixels() - 1) * 2;
-        Color1 = color1;
-        Index = 0;
+      ActivePattern = SCANNER;
+      Interval = interval;
+      TotalSteps = (numPixels() - 1) * 2;
+      Color1 = color1;
+      Index = 0;
     }
 
     // Update the Scanner Pattern
     void ScannerUpdate()
-    { 
-        for (int i = 0; i < numPixels(); i++)
+    {
+      for (int i = 0; i < numPixels(); i++)
+      {
+        if (i == Index)  // Scan Pixel to the right
         {
-            if (i == Index)  // Scan Pixel to the right
-            {
-                 setPixelColor(i, Color1);
-            }
-            else if (i == TotalSteps - Index) // Scan Pixel to the left
-            {
-                 setPixelColor(i, Color1);
-            }
-            else // Fading tail
-            {
-                 setPixelColor(i, DimColor(getPixelColor(i)));
-            }
+          setPixelColor(i, Color1);
         }
-        show();
-        Increment();
+        else if (i == TotalSteps - Index) // Scan Pixel to the left
+        {
+          setPixelColor(i, Color1);
+        }
+        else // Fading tail
+        {
+          setPixelColor(i, DimColor(getPixelColor(i)));
+        }
+      }
+      show();
+      Increment();
     }
-    
+
     // Calculate 50% dimmed version of a color (used by ScannerUpdate)
     uint32_t DimColor(uint32_t color)
     {
-        // Shift R, G and B components one bit to the right
-        uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
-        return dimColor;
+      // Shift R, G and B components one bit to the right
+      uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
+      return dimColor;
     }
 
     // Set all pixels to a color (synchronously)
     void ColorSet(uint32_t color)
     {
-        for (int i = 0; i < numPixels(); i++)
-        {
-            setPixelColor(i, color);
-        }
-        show();
+      for (int i = 0; i < numPixels(); i++)
+      {
+        setPixelColor(i, color);
+      }
+      show();
     }
 
     // Returns the Red component of a 32-bit color
     uint8_t Red(uint32_t color)
     {
-        return (color >> 16) & 0xFF;
+      return (color >> 16) & 0xFF;
     }
 
     // Returns the Green component of a 32-bit color
     uint8_t Green(uint32_t color)
     {
-        return (color >> 8) & 0xFF;
+      return (color >> 8) & 0xFF;
     }
 
     // Returns the Blue component of a 32-bit color
     uint8_t Blue(uint32_t color)
     {
-        return color & 0xFF;
+      return color & 0xFF;
     }
-    
+
     // Input a value 0 to 255 to get a color value.
     // The colours are a transition r - g - b - back to r.
     uint32_t Wheel(byte WheelPos)
     {
-        WheelPos = 255 - WheelPos;
-        if(WheelPos < 85)
-        {
-            return Color(255 - WheelPos * 3, 0, WheelPos * 3);
-        }
-        else if(WheelPos < 170)
-        {
-            WheelPos -= 85;
-            return Color(0, WheelPos * 3, 255 - WheelPos * 3);
-        }
-        else
-        {
-            WheelPos -= 170;
-            return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-        }
+      WheelPos = 255 - WheelPos;
+      if (WheelPos < 85)
+      {
+        return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+      }
+      else if (WheelPos < 170)
+      {
+        WheelPos -= 85;
+        return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+      }
+      else
+      {
+        WheelPos -= 170;
+        return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+      }
     }
 };
 
@@ -216,45 +216,45 @@ NeoPatterns Ring1(24, 9, NEO_GRB + NEO_KHZ800, &Ring1Complete);
 void setup()
 {
   Serial.begin(115200);
-  
-    pinMode(solenoidPin, OUTPUT);  
-    pinMode(detectorPin, INPUT);
-    pinMode(6, INPUT_PULLUP);
-   
-    digitalWrite(solenoidPin, HIGH);
-    delay(3000);
-    digitalWrite(solenoidPin, LOW);
-   
-   // Initialize all the pixelStrips
-    Ring1.begin();
-    
-    // Kick off a pattern
-    Ring1.Scanner(Ring1.Color(255,0,0), 50);
+
+  pinMode(solenoidPin, OUTPUT);
+  pinMode(detectorPin, INPUT);
+  pinMode(6, INPUT_PULLUP);
+
+  digitalWrite(solenoidPin, HIGH);
+  delay(3000);
+  digitalWrite(solenoidPin, LOW);
+
+  // Initialize all the pixelStrips
+  Ring1.begin();
+
+  // Kick off a pattern
+  Ring1.Scanner(Ring1.Color(255, 0, 0), 50);
 }
 
 // Main loop
 void loop()
 {
-    // Update the rings.
-    Ring1.Update();
-    Serial.println(digitalRead(6));
-    // Switch patterns on a button press:
-    if (digitalRead(6) == 1) // Button #1 pressed
-    {
-        Ring1.Color1 = Ring1.Wheel(50);
-        Ring1.TotalSteps = Ring1.numPixels();
-        Ring1.Interval = 100;
-        Ring1.ActivePattern = COLOR_WIPE;
-    }
-    else // Back to normal operation
-    {
-        // Restore all pattern parameters to normal values
-        Ring1.Interval = 50;
-        Ring1.Color1 = Ring1.Wheel(0);
-        Ring1.TotalSteps = Ring1.numPixels();
-        Ring1.ActivePattern = SCANNER;
-        
-    }    
+  // Update the rings.
+  Ring1.Update();
+  Serial.println(digitalRead(6));
+  // Switch patterns on a button press:
+  if (digitalRead(6) == 1) // Button #1 pressed
+  {
+    Ring1.Color1 = Ring1.Wheel(50);
+    Ring1.TotalSteps = Ring1.numPixels();
+    Ring1.Interval = 100;
+    Ring1.ActivePattern = COLOR_WIPE;
+  }
+  else // Back to normal operation
+  {
+    // Restore all pattern parameters to normal values
+    Ring1.Interval = 50;
+    Ring1.Color1 = Ring1.Wheel(0);
+    Ring1.TotalSteps = Ring1.numPixels();
+    Ring1.ActivePattern = SCANNER;
+
+  }
 }
 
 //------------------------------------------------------------
@@ -264,13 +264,20 @@ void loop()
 // Ring1 Completion Callback
 void Ring1Complete()
 {
-    if (digitalRead(6) == 1)  // Button #2 pressed
-    {
-      digitalWrite(solenoidPin, HIGH);
-    }
-    else  // Retrn to normal
-    {
-      Ring1.Reverse();
-    }
+  if (digitalRead(6) == 1)  // Button #2 pressed
+  {
+    Ring1.ColorSet(Ring1.Wheel(50));
+    digitalWrite(solenoidPin, HIGH);
+    delay(15000);
+    Ring1.Interval = 50;
+    Ring1.Color1 = Ring1.Wheel(0);
+    Ring1.TotalSteps = Ring1.numPixels();
+    Ring1.ActivePattern = SCANNER;
+    digitalWrite(solenoidPin, LOW);
+  }
+  else  // Retrn to normal
+  {
+    Ring1.Reverse();
+  }
 }
 
